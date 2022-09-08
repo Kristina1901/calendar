9097fba6e220bb0db393efffa5c1f687ba93dcbd
Kristina Tuzova <ktuzova1901@gmail.com>
2022-09-09 01:34:39 +0300

    feat: adding styles and component Datepicker

--- src/components/components/Datepicker/Datepicker.jsx
+++ src/components/components/Datepicker/Datepicker.jsx
@@ -1,9 +1,7 @@
-import { useState, useEffect, useRef} from 'react';
-import moment from 'moment';
 import useCalendar from '../../hooks/useCalendar';
-import s from './Datepicker.module.css'
-let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-const DatePicker = () => {
+import s from './Datepicker.module.css';
+let days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+const DatePicker = ({ date, setDate }) => {
   const {
     calendarRows,
     selectedDate,
@@ -12,25 +10,26 @@ const DatePicker = () => {
     getNextMonth,
     getPrevMonth,
   } = useCalendar();
-  const [date, setDate] = useState(null);
-   
- 
+
   return (
     <div className={s.container}>
       <div className={s.month}>
-        
         <div className={s.wrapperButton}>
-        <button className={s.prev} onClick={getPrevMonth}>
-        </button>
-        <p>{`${
-         monthNames[selectedDate.getMonth()]
-        } ${selectedDate.getFullYear()}`}</p>
-        <button className={s.next} onClick={getNextMonth}>
-        </button>
+          <button className={s.prev} onClick={getPrevMonth}></button>
+          <p>{`${
+            monthNames[selectedDate.getMonth()]
+          } ${selectedDate.getFullYear()}`}</p>
+          <button className={s.next} onClick={getNextMonth}></button>
         </div>
-        </div>
+      </div>
       <table className={s.table}>
-        <thead><tr>{days.map(item =>(<td key={item}>{item}</td>))}</tr></thead>
+        <thead>
+          <tr>
+            {days.map(item => (
+              <td key={item}>{item}</td>
+            ))}
+          </tr>
+        </thead>
         <tbody>
           {Object.values(calendarRows).map(cols => {
             return (
@@ -39,30 +38,24 @@ const DatePicker = () => {
                   col.date === todayFormatted ? (
                     <td
                       key={todayFormatted}
-                      className={
-                        s.cell            
-                      }
-                      onClick={()=>setDate(col.date)}
-                     
+                      className={s.cell}
+                      onClick={() => setDate(col.date)}
                     >
                       <div className={s.info}>
                         <p>{col.value}</p>
-                        </div>
-                      <p >{col.text}</p>
+                      </div>
+                      <p>{col.text}</p>
                     </td>
                   ) : (
                     <td
                       key={col.date}
-                      className={
-                        s.cell 
-                     }
-                     onClick={()=>setDate(col.date)}
-                     
+                      className={s.cell}
+                      onClick={() => setDate(col.date)}
                     >
                       <div className={s.info}>
                         <p>{col.value}</p>
-                        </div>
-                      </td>
+                      </div>
+                    </td>
                   )
                 )}
               </tr>
@@ -70,7 +63,7 @@ const DatePicker = () => {
           })}
         </tbody>
       </table>
-      </div>
-  )
-}
-export default DatePicker
+    </div>
+  );
+};
+export default DatePicker;
