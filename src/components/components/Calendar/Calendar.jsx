fe9c09963110a1481d316fc12d0422c1e1e03e10
Kristina Tuzova <ktuzova1901@gmail.com>
2022-09-09 01:34:01 +0300

    feat: adding function for modal and form

--- src/components/components/Calendar/Calendar.jsx
+++ src/components/components/Calendar/Calendar.jsx
@@ -3,7 +3,7 @@ import moment from 'moment';
 import s from './Calendar.module.css';
 import Modal from '../Modal/Modal';
 import DatePicker from '../Datepicker/Datepicker';
-import { useState, useEffect, useRef} from 'react';
+import { useState, useEffect, useRef } from 'react';
 const Calendar = () => {
   const {
     today,
@@ -15,117 +15,122 @@ const Calendar = () => {
     getPrevMonth,
   } = useCalendar();
   const refContainer = useRef(calendarRows);
-    const [open, setOpen] = useState(false);
-    const [array, setArray] = useState({});
-    const [dropdown, setdropdown] = useState(false);
-    
-    useEffect(() => {
-      if (Object.keys(array).length === 0) {
-        setArray(refContainer.current)
-      }
-    //    if(localStorage.getItem(today.getMonth()) !== JSON.stringify(array) && selectedDate.getMonth()=== today.getMonth()) {
-    //    setArray(JSON.parse(localStorage.getItem(today.getMonth())))
-    //    console.log('VVV')
-    //  }
-    //   if(refContainer.current!== calendarRows && selectedDate.getMonth()!== today.getMonth()) {
-    //     let k = JSON.parse(localStorage.getItem(selectedDate.getMonth().toString()))
-    //     if (k === null){
-    //       setArray(calendarRows)
-    //     }
-    //     else {
-    //       setArray(k)
-    //     }
-      
-    //  }
-                    
-    //  if(JSON.stringify(array) === JSON.stringify(refContainer.current) && selectedDate.getMonth()=== today.getMonth()) {
-    //    setArray(JSON.parse(localStorage.getItem(today.getMonth().toString())))
-    //    console.log('Hi')
-    // } 
-     
-     // if(JSON.stringify(array) !== JSON.stringify(refContainer.current) && selectedDate.getMonth()=== today.getMonth()) {
-     //   localStorage.setItem(`${today.getMonth()}`, JSON.stringify(array))
-     //    console.log('hhhh')
-     
-       
-             
-      
-     // }
-     // if(selectedDate.getMonth()=== today.getMonth() && JSON.stringify(copy) !== JSON.stringify(refContainer)) {
-     //    setArray(copy)
-     //    console.log('y')
-     // }
-   //   if(JSON.stringify(array) !== JSON.stringify(refContainer.current)&& selectedDate.getMonth()=== today.getMonth()) {
-   //    setArray(refContainer.current)
-   //    console.log('lkji')
-   //  }
-    
-     
+  const [open, setOpen] = useState(false);
+  const [array, setArray] = useState(calendarRows);
+  const [dropdown, setdropdown] = useState(false);
+  const [date, setDate] = useState(null);
+  const [disabled, setDisable] = useState(false);
+  const [some, setSome] = useState(false);
+  const[fieldTitle, setFieltitle] = useState('')
+  const[fieldMessage, setFielddMessage] = useState('')
+  const[fieldDay, setFielddDay] = useState('')
+  const[fieldTime, setFielddTime] = useState('')
 
-
-   },[calendarRows,today,selectedDate, array]);
-  const findKey = (key, title, time) => {
-  let el = Object.values(calendarRows).flat().find(item =>(item.date === key))
-  // localStorage.setItem(`${time}`, JSON.stringify(add()))
-  // console.log(add())
-  setArray(prev => {
-    const newState = Object.values(prev).map(el => cheaking(el))
-    function cheaking(item) {
-       let array = item.map(function(num){
-      
-       if (num.date === el.date) {
-        return num = {
-            ...num,        
-           text: title}
+  useEffect(() => {
+    if(array=== refContainer.current && localStorage.getItem(today.getMonth()) === null) {
+      return
     }
-    else {
-      return num = {
-        ...num        
-      }
-
+    if (date !== null) {
+      setDisable(true);
+      setdropdown(false);
     }
-     })
+    if (
+      localStorage.getItem(today.getMonth()) !== JSON.stringify(array) &&
+      selectedDate.getMonth() === today.getMonth()
+    ) {
+      setArray(JSON.parse(localStorage.getItem(today.getMonth())));
      
-    return array
-    
+    }
+    if (
+      refContainer.current !== calendarRows &&
+      selectedDate.getMonth() !== today.getMonth()
+    ) {
+      let k = JSON.parse(
+        localStorage.getItem(selectedDate.getMonth().toString())
+      );
+      if (k === null) {
+        setArray(calendarRows);
+      } else {
+        setArray(k);
+      }
+    }
    
-    
-} 
+  }, [selectedDate,date]);
+  const findKey = (key, title, month, message, tictac) => {
+    let el = Object.values(calendarRows)
+      .flat()
+      .find(item => item.date === key);
+    setArray(prev => {
+      const newState = Object.values(prev).map(el => cheaking(el));
+      function cheaking(item) {
+        let array = item.map(function (num) {
+          if (num.date === el.date) {
+            return (num = {
+              ...num,
+              text: title,
+              info: message,
+              hours: tictac
 
-let k = {...newState}
-localStorage.setItem(`${time}`, JSON.stringify(k))
-return {...newState}      
- 
-  
-})
-}
+            });
+          } else {
+            return (num = {
+              ...num,
+            });
+          }
+        });
+
+        return array;
+      }
+
+      let k = { ...newState };
+      localStorage.setItem(`${month}`, JSON.stringify(k));
+      return { ...newState };
+    });
+  };
 
   function closeModal() {
+    setSome(false)
     setOpen(!open);
-   
   }
   function openDropdown() {
-    setdropdown(true)
-   
+    setdropdown(true);
   }
-   return (
+  function getFormField(item) {
+    if(item.text!==undefined && item.text!=='') {
+      closeModal()
+      setSome(true)
+      setFieltitle(item.text)
+      setFielddMessage(item.info)
+      setFielddDay(item.date)
+      setFielddTime(item.hours)
+         
+    }
+    else {
+     return
+    }
+    
+    
+  }
+  return (
     <div className={s.container}>
       <div className={s.month}>
         <div>
-        <button className={s.event} onClick={closeModal}></button>
+          <button className={s.event} onClick={closeModal}></button>
         </div>
         <div className={s.wrapperButton}>
-        <button className={s.prev} onClick={getPrevMonth}>
-        </button>
-        <p>{`${
-         monthNames[selectedDate.getMonth()]
-        } ${selectedDate.getFullYear()}`}</p>
-        <button className={s.next} onClick={getNextMonth}>
-        </button>
-        <button className={s.pick} onClick={openDropdown}></button>
-        {dropdown && <DatePicker/>}
+          <button className={s.prev} onClick={getPrevMonth}></button>
+          <p>{`${
+            monthNames[selectedDate.getMonth()]
+          } ${selectedDate.getFullYear()}`}</p>
+          <button className={s.next} onClick={getNextMonth}></button>
+          <button
+            className={s.pick}
+            onClick={openDropdown}
+            disabled={disabled}
+          ></button>
+          {dropdown && <DatePicker date={date} setDate={setDate} />}
         </div>
-        </div>
+      </div>
       <table className={s.table}>
         <tbody>
           {Object.values(array).map(cols => {
@@ -133,45 +138,24 @@ return {...newState}
               <tr key={cols[0].date}>
                 {cols.map(col =>
                   col.date === todayFormatted ? (
-                    <td
-                      key={todayFormatted}
-                      className={
-                        s.today              
-                      }
-                     
-                    >
+                    <td key={todayFormatted} className={s.today}>
                       <div className={s.info}>
                         <p>{col.value}</p>
                         {col.value && (
-                          <p>
-                            {moment(
-                              new Date(col.date)
-                            ).format('dd')}
-                           
-                          </p>
+                          <p>{moment(new Date(col.date)).format('dd')}</p>
                         )}
                       </div>
-                      <p >{col.text}</p>
+                      <p>{col.text}</p>
                     </td>
                   ) : (
-                    <td
-                      key={col.date}
-                      className={
-                        s.cell 
-                     }
-                     
-                    >
+                    <td key={col.date} className={s.cell} onClick={()=> getFormField(col)}>
                       <div className={s.info}>
                         <p>{col.value}</p>
                         {col.value && (
-                          <p>
-                            {moment(
-                              new Date(col.date)
-                            ).format('dd')}
-                          </p>
+                          <p>{moment(new Date(col.date)).format('dd')}</p>
                         )}
                       </div>
-                       <p className={s.notice}>{col.text}</p>
+                      <p className={s.notice}>{col.text}</p>
                     </td>
                   )
                 )}
@@ -180,7 +164,8 @@ return {...newState}
           })}
         </tbody>
       </table>
-      <Modal trigger={open} onClose={closeModal} findKey={findKey}/>
+      <Modal trigger={open} onClose={closeModal} findKey={findKey} some={some} 
+      fieldTitle={fieldTitle} fieldMessage={fieldMessage} fieldDay={fieldDay} fieldTime={fieldTime}/>
     </div>
   );
 };
