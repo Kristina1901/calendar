d7ca43948226e23c0407219c73a9c7e1428ab686
Kristina Tuzova <ktuzova1901@gmail.com>
2022-09-09 13:55:57 +0300

    date

--- src/components/components/Calendar/Calendar.jsx
+++ src/components/components/Calendar/Calendar.jsx
@@ -116,9 +116,7 @@ const Calendar = () => {
         </div>
         <div className={s.wrapperButton}>
           <button className={s.prev} onClick={getPrevMonth}></button>
-          <p className={s.currentDate}>{`${
-            monthNames[selectedDate.getMonth()]
-          } ${selectedDate.getFullYear()}`}</p>
+          <p className={s.currentDate}>{`${moment(array[1][0].date).format('MMMM  YYYY')}`}</p>
           <button className={s.next} onClick={getNextMonth}></button>
           <button
             className={s.pick}
